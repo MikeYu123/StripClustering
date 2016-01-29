9f17817c5435b92291216776efacc50cd5652bd3
Mike Yurchenkov <mikesehse@gmail.com>
2016-01-29 21:11:04 +0300

    M-step routines added

--- src/KMeansClusterer.scala
+++ src/KMeansClusterer.scala
@@ -10,12 +10,15 @@ class KMeansClusterer (k: Int){
     List(1 to k) map (x => new Line(randDouble(1, -1), randDouble(30, -30)))
   }
 
-  def pickLines(lines: List[Line], points: List[Point]) : Map[Line, List[Point]] = {
-    points groupBy(x => pickLine(x, lines))
-  }
-
   def resetLine(points: List[Point]) : Line = {
-
+    val n = points.length
+    val sumxy = (points map (x => x.x * x.y)) reduce ((x, y) => x+y)
+    val sumx = points reduce ((x,y) => x.x + y.x)
+    val sumy = points reduce ((x,y) => x.y + y.y)
+    val sumxsq = points reduce ((x,y) => (x.x*x.x) + (y.x * y.x))
+    val k = ((n * sumxy) - (sumx * sumy)) / ((n * sumxsq) - (sumx * sumx))
+    val b = (sumy - (k * sumx)) / n
+    new Line(k, b)
   }
 
   def pickLine(point: Point, lines: List[Line]):Line = {
@@ -24,6 +27,14 @@ class KMeansClusterer (k: Int){
       })
   }
 
+  def pickLines(lines: List[Line], points: List[Point]) : Map[Line, List[Point]] = {
+    points groupBy(x => pickLine(x, lines))
+  }
+
+  def resetLines(lineMap : Map[Line, List[Point]]) : List[Line] = {
+//    TODO: check if tolist is efficient
+    lineMap.values.toList map (points => resetLine(points))
+  }
 
 }
 
