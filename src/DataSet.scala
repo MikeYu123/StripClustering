1a84e8743204340fbb242c7fe1d0a185116c6bdb
Максим Яковлев <m.yakovlev@maximatelecom.ru>
2016-01-29 22:36:20 +0300

    maven

---
+++ src/DataSet.scala
@@ -0,0 +1,17 @@
+import java.io.File
+
+import com.fasterxml.jackson.core.{JsonParser, JsonFactory}
+
+object DataSet {
+  val str = "Ololo"
+  println("hello world")
+  var jfactory = new JsonFactory()
+
+
+  /*** read from file ***/
+  var jParser = jfactory.createJsonParser(new File(".\\data\\data.json"))
+
+  def main(args: Array[String]) {
+    println(this.str)
+  }
+}
