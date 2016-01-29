e074ef65c10dbcb9f61123969b72202671f0ab5b
Максим Яковлев <m.yakovlev@maximatelecom.ru>
2016-01-30 00:13:08 +0300

    DataSet implemented

--- src/DataSet.scala
+++ src/DataSet.scala
@@ -3,8 +3,21 @@ import org.json4s._
 import org.json4s.jackson.JsonMethods._
 
 object DataSet {
-  val sep = File.separator
-  val data = parse(new File(List(".", "src","data", "data.json").mkString(sep)))
+  private val sep = File.separator
+  private val rawData:JValue = parse(new File(List(".", "src","data", "data.json").mkString(sep)))
+
+  val data = for {
+    JArray(data) <- rawData
+    JObject(item) <- data
+    JField("name", JString(name)) <- item
+    JField("address", JString(address)) <- item
+    JField("location", JObject(location)) <- item
+    JField("lat", JDouble(lat)) <- location
+    JField("lng", JDouble(lng)) <- location
+
+  } yield Point(lat, lng, address, name)
+
+  println(data)
 
   def main(args: Array[String]) {
 
--- src/Point.scala
+++ src/Point.scala
@@ -2,4 +2,4 @@
  * Created by mayur_000 on 29.01.2016.
  */
 //Suppose we have flat earth map with x and y as coordinates
-case class Point(x: Double, y: Double)
+case class Point(x: Double, y: Double, street: String, name: String)
